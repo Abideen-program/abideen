9e45374a5625775dcf817936f4ce3ecf401deb27
Abideen Olafimihan <olafimihana@gmail.com>
2024-01-09 18:30:44 +0100

    navbar done remaining light/dark toggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
+import { GithubIcon, LinkedIn, TwitterIcon } from "./Icons";
+import { motion } from "framer-motion";
 
 const CustomLink = ({ href, title, className }: CustomLink) => {
   const { asPath } = useRouter();
@@ -27,21 +29,39 @@ const Navbar = () => {
         <CustomLink href="/projects" title="Projects" className="ml-4" />
       </nav>
 
-      <ul className="flex">
+      <ul className="flex items-center justify-center gap-5">
+        <motion.a
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          href="https://twitter.com/Olafimihanabid"
+          target="_blank"
+        >
+          <li className="w-6">
+            <TwitterIcon />
+          </li>
+        </motion.a>
+        <motion.a
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          href="https://github.com/Abideen-program"
+          target="_blank"
+        >
+          <li className="w-6">
+            <GithubIcon />
+          </li>
+        </motion.a>
+        <motion.a
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          href="https://www.linkedin.com/in/olafimihan-abideen/"
+          target="_blank"
+        >
+          <li className="w-6">
+            <LinkedIn />
+          </li>
+        </motion.a>
         <Link href="/" target="_blank">
-          <li>T</li>
-        </Link>
-        <Link href="/" target="_blank">
-          <li>T</li>
-        </Link>
-        <Link href="/" target="_blank">
-          <li>T</li>
-        </Link>
-        <Link href="/" target="_blank">
-          <li>T</li>
-        </Link>
-        <Link href="/" target="_blank">
-          <li>T</li>
+          <li>{/* <LightDarkIcon /> */}</li>
         </Link>
       </ul>
       <div className="absolute left-[50%] translate-x-[-50%]">
