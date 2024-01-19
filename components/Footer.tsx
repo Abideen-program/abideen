2f0a5a16028e14c853a590b9d0d1887fa4b9cee0
Abideen Olafimihan <olafimihana@gmail.com>
2024-01-19 01:51:21 +0100

    making fotter responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,11 @@ import Layout from "./Layout";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-dark dark:border-light w-full font-medium text-lg dark:text-light 3xl:text-4xl">
-      <Layout className="!p-8 lg:!px-32 flex items-center justify-between">
+    <footer className="border-t-2 border-dark dark:border-light w-full font-medium dark:text-light 3xl:text-4xl">
+      <Layout className="!p-8 lg:!px-32 flex flex-col items-center justify-between lg:flex-row gap-3 lg:gap-0">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <div className="flex items-center">
-          Build with <span className="text-lg text-primary dark:text-primaryDark px-1">&#9825;</span>{" "}
+          Build with <span className="text-lg 3xl:text-6xl text-primary dark:text-primaryDark px-1">&#9825;</span>{" "}
           <Link href="/" className="underline underline-offset-2">
             DevDeen
           </Link>
