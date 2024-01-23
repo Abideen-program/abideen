a289bb06097b0bf7f914f500e4c316bfd11fc832
Abideen Olafimihan <olafimihana@gmail.com>
2024-01-23 10:16:48 +0100

    all work done

--- components/TransitionEffects.tsx
+++ components/TransitionEffects.tsx
@@ -7,21 +7,21 @@ const TransitionEffects = () => {
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: 0, width: 0 }}
         transition={{ duration: 0.8, ease: easeInOut }}
+        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
         className="fixed top-0 bottom-0 right-full w-screen h-screen bg-primaryDark z-[50]"
       />
       <motion.div
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: 0, width: 0 }}
-        transition={{ delay:0.2, duration: 0.8, ease: easeInOut }}
+        transition={{ delay: 0.2, duration: 0.8, ease: easeInOut }}
         className="fixed top-0 bottom-0 right-full w-screen h-screen bg-light z-[40]"
       />
       <motion.div
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: 0, width: 0 }}
-        transition={{ delay:0.4, duration: 0.8, ease: easeInOut }}
+        transition={{ delay: 0.4, duration: 0.8, ease: easeInOut }}
         className="fixed top-0 bottom-0 right-full w-screen h-screen bg-primary z-[30]"
       />
-      
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { Montserrat } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -11,12 +13,17 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const {asPath} = useRouter()
+
   return (
     <main
       className={`${montserrat.className} bg-light min-h-screen dark:bg-dark relative`}
     >
       <Navbar />
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <Component key={asPath} {...pageProps} />
+      </AnimatePresence >
       <div className="absolute right-8 md:left-8 bottom-[180px] lg:left-auto lg:right-8 lg:bottom-28 3xl:bottom-[180px] 3xl:right-[130px]">
         <ScrollToTop />
       </div>
